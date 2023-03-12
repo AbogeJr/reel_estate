71856a643dec0a0cfdc865e9d6374e961f8ee22b
AbogeJr <junioraboge@gmail.com>
2023-03-12 14:37:40 +0300

    fix: property Card

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -2,7 +2,7 @@ import PropertyCard from "./PropertyCard";
 
 const SideBar = ({ location, onClose }) => {
   return (
-    <div className="font-mono overflow-scroll relative bg-white md:w-2/5 right-0 ml-auto mx-4 flex  flex-col  z-30  items-center rounded-lg ">
+    <div className="font-mono overflow-scroll relative bg-white md:w-2/5 top-24 right-0 ml-auto mx-4 flex  flex-col  z-30  items-center rounded-lg ">
       <button
         onClick={onClose}
         className="text-2xl  rounded-lg p-5 top-0  left-0 self-start hover:text-red-500 hover:scale-125"
