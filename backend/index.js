174e0666d18a5f876228485cab9f8f51802eb473
AbogeJr <junioraboge@gmail.com>
2023-03-13 21:48:42 +0300

    update: docs

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 // Import libs
 const express = require("express");
+// Function for connecting to database
 const { dbConn } = require("./config");
+// Cross Origin Requests
 const cors = require("cors");
+// Importing routes
 const { router } = require("./routes/routes");
 // Connect to database
 dbConn();
@@ -10,7 +13,6 @@ const app = express();
 
 //Middleware for form data
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
 
 // Enabling CORS
@@ -19,6 +21,7 @@ app.use(cors());
 // Port for the server
 const port = 5000;
 
+// Execute router from ../routes
 app.use(router);
 
 app.listen(port, () => {
