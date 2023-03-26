a48e628f2f46d3bb0e5cdd45a5030fb025adb04b
AbogeJr <junioraboge@gmail.com>
2023-03-26 21:10:53 +0300

    update: remove signalling server

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,6 @@ dbConn();
 // Initalizing the app
 const app = express();
 
-const http = require("http");
-
 //Middleware for form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,35 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 // Enabling CORS
 app.use(cors());
 
-const server = http.createServer(app);
-
-// For signalling server
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
-
 // Port for the server
 const port = 5000;
 
 // Execute router from ../routes
 app.use(router);
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-});
-
-io.on("disconnect", (socket) => {
-  console.log("user disconnected");
-});
-
-const PORT = process.env.PORT || 5500;
-
-server.listen(PORT, () => {
-  console.log(`Signaling server listening on http://localhost:${PORT}`);
-});
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
